814d1e76cfbba06b914a60a96e7813ffeb0cc2d2
yuvrajkrishna <yuvrajkrishna26@gmail.com>
2026-06-18 22:01:08 +0530

    snake default location added

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 const board = document.querySelector('.board')
-const blockHeight = 40
-const blockWidth = 40
+const blockHeight = 50
+const blockWidth = 50
 let cols = Math.floor(board.clientWidth/blockWidth)
 let rows = Math.floor(board.clientHeight/blockHeight)
 
-for(let i = 0 ; i < rows ;  i++){
-    for(let j = 0 ; j < cols ; j++){
-        let blocks = document.createElement('div')
-        blocks.classList.add('blocks')
-        board.appendChild(blocks)
+const blocks = []
+
+for(let row = 0 ; row < rows ;  row++){
+    for(let col = 0 ; col < cols ; col++){
+        let block = document.createElement('div')
+        block.classList.add('blocks')
+        board.appendChild(block)
+        blocks[`${row}-${col}`] = block;
+        block.innerText = [`${row}-${col}`];
     }
 }
+
+let snake = [{x:2,y:3}]
+
+snake.forEach(element=>{
+    blocks[`${element.x}-${element.y}`].classList.add('fill')
+})
